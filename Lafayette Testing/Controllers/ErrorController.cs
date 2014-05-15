dedd5d06f532765cb8e9d42c781c345ec1248a32
James Newell <jnewell@laughlin.com>
2014-05-14 22:38:20 -0500

    Added custom error page for 404 and 500 errors.

---
+++ Lafayette Testing/Controllers/ErrorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lafayette_Testing.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Redirect("~/");
+        }
+
+        [ActionName("http-error")]
+        public ActionResult HttpError()
+        {
+            return View("HttpError");
+        }
+
+        [ActionName("page-not-found")]
+        public ActionResult PageNotFound()
+        {
+            return View("PageNotFound");
+        }
+
+        [ActionName("internal-server-error")]
+        public ActionResult InternalServerError()
+        {
+            return View("InternalServerError");
+        }
+    }
+}
