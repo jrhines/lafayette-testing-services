642b72afa227e751840fd43512d64881f4b94ebd
Jim Rhines <jrhines@laughlin.com>
2014-06-14 23:30:26 -0500

    Script update to use image alt for Fancybox caption in galleries.

--- Lafayette Testing/Scripts/site.js
+++ Lafayette Testing/Scripts/site.js
@@ -107,6 +107,15 @@ $(function () {
 		closeEffect: 'none'
 	});
 
+	//use the alt tag of an image to display the fancybox driven caption
+	$(".fancybox").fancybox({
+		beforeShow: function () {
+			var alt = this.element.find('img').attr('alt');
+			this.inner.find('img').attr('alt', alt);
+			this.title = alt;
+		}
+	});
+
 	// fancybox video
 	//if (isMobile) {
 		//$('.fancybox-video').on('click tap', function () { return window.open($(this).attr('href')), !1 });
